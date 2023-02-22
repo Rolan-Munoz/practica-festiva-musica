d89eee55c6d36678ffb1791d59329e0b97f9f05e
Rolan Muñoz <rolanmunoz@MacBook-Air-de-Rolan.local>
2023-02-22 03:17:24 +0100

    optimizacion img

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
-const {src, dest, watch} = require('gulp');
+const {src, dest, watch, parallel} = require('gulp');
+
+//CSS
 const sass = require('gulp-sass')(require('sass'));  //creamos la funcion sass importando la dependencia de sass para 
 //   trabajar con el en la compilacion, gulp-sass sirve para conectarlos a ambos
 const plumber = require('gulp-plumber');
+
+//IMG
+const imagemin = require('gulp-imagemin');
+const cache = require('gulp-cache');
+const webp = require('gulp-webp');
 
 async function css () {
 
@@ -9,16 +16,36 @@ async function css () {
     .pipe(plumber())            // evitar que se detenga el workflow al tener errores.
     .pipe(sass())               //compilarlo
     .pipe(dest('build/css'));   //guardarlo en el disco
+};
+
+async function imagenes () {
+    const opciones = {
+        optimizationLevel: 3
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe(cache(imagemin(opciones)))
+        .pipe(dest('build/img'));
+
 }
 
+async function imgWebp () {
 
-exports.css = css;  // se llama a la funcion a traves de node js por eso la sintaxis diferente a js
+    const opciones = {
+        quality: 50
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'));
+};
 
 
 async function dev() {
     watch('src/scss/**/*.scss', css) //con watch escuchamos los cambios producidos  y luego ejecutamos css
-}
+};
 
 
-    
-exports.dev = dev;  
+exports.css = css;  // se llama a la funcion a traves de node js por eso la sintaxis diferente a js
+exports.imagenes = imagenes;
+exports.imgWebp = imgWebp;
+exports.dev = parallel(imagenes, imgWebp, dev);
+
